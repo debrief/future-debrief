64dac2bdb96d84247e5056dd95049b61faab0d6c
Ian Mayo <ian@planetmayo.com>
2025-08-22 12:34:30 +0100

    feat: automate sidebar icon copying during build process
    - Add copy-webpack-plugin to copy debrief-icon.svg to media/
    - Icon is now generated during build rather than manually tracked
    - Ensures sidebar icon is always available for VS Code extension
    - Remove manual vite plugin approach in favor of webpack solution

--- vs-code/webpack.config.js
+++ vs-code/webpack.config.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
@@ -40,6 +41,13 @@ const extensionConfig = {
       }
     ]
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'debrief-icon.svg', to: '../media/debrief-icon.svg' }
+      ]
+    })
+  ],
   devtool: 'nosources-source-map',
   infrastructureLogging: {
     level: "log", // enables logging required for problem matchers
