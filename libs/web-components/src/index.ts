825a3bd4189582ce8c9c9c95f23df442300899f1
Ian Mayo <ian@planetmayo.com>
2025-09-24 17:21:16 +0100

    feat: add ToolExecuteButton and its type exports for improved component accessibility

--- libs/web-components/src/index.ts
+++ libs/web-components/src/index.ts
@@ -5,9 +5,11 @@ export { MapComponent } from './MapComponent/MapComponent';
 
 export { CurrentStateTable } from './CurrentStateTable/CurrentStateTable';
 export { OutlineView } from './OutlineView/OutlineView';
+export { ToolExecuteButton } from './ToolExecuteButton/ToolExecuteButton';
 
 // Type exports
 export type { TimeControllerProps } from './TimeController/TimeController';
 export type { PropertiesViewProps } from './PropertiesView/PropertiesView';
 export type { MapComponentProps, GeoJSONFeature, GeoJSONFeatureCollection, MapState } from './MapComponent/MapComponent';
 export type { OutlineViewProps, ToolIndex } from './OutlineView/OutlineView';
+export type { ToolExecuteButtonProps, SelectedCommand } from './ToolExecuteButton/ToolExecuteButton';
